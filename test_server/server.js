a71f60e0af4a6e3e09b83e707991a7aba3a1c2be
Miguel Sanchez <migchez@umich.edu>
2014-09-01 15:54:40 -0400

    Changed test server to use relative path:

--- test_server/server.js
+++ test_server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var server = express();
-server.use(express.static('/Users/migchez/Wisely/CommandCenter/app/'));
+server.use(express.static(__dirname.substring(0,37) + '/app/'));
 
 var port = Number (process.env.PORT || 5000);
 server.listen(port, function () {
